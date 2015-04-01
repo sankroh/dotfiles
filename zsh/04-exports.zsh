a7d3db6662823252c4b9917140cada3a6ce97add
Rohit Sankaran <rohit.sankaran@gmail.com>
2015-03-31 21:29:45 -0500

    Added coreutils and findutils binaries to path

--- zsh/04-exports.zsh
+++ zsh/04-exports.zsh
@@ -1,11 +1,11 @@
 # Currently this path is appended to dynamically when picking a ruby version
 # zshenv has already started PATH with rbenv so append only here
 #export PATH=$PATH:/usr/local/bin:/usr/local/sbin:$HOME/bin
-my_path=$HOME/bin
+my_path=$HOME/.bin
 local_path=/usr/local/bin:/usr/local/sbin
-pgsql_path=/Applications/Postgres93.app/Contents/MacOS/bin
 heroku_path=/usr/local/heroku/bin
-export PATH=$heroku_path:$pgsql_path:$local_path:$PATH:$my_path
+coreutils_path=$(brew --prefix coreutils)/libexec/gnubin
+export PATH=$coreutils_path:$heroku_path:$local_path:$PATH:$my_path
 
 # remove duplicate entries
 typeset -U PATH
